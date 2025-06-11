a8014262c5b997cd8b137a35d8c268ca4c2eeb10
reganputra <ramadhanregan334@gmail.com>
2025-06-12 06:11:18 +0700

    add users table to database schema

--- connection/database.go
+++ connection/database.go
@@ -24,6 +24,17 @@ func DbConnect() {
 }
 
 func createTable() error {
+	createUsersTable := `
+CREATE TABLE IF NOT EXISTS users (
+    	id INTEGER PRIMARY KEY AUTOINCREMENT,
+    	email TEXT NOT NULL UNIQUE,
+    	password TEXT NOT NULL
+);`
+	_, err := DB.Exec(createUsersTable)
+	if err != nil {
+		return err
+	}
+
 	createEventsTable := `
 	CREATE TABLE IF NOT EXISTS events (
 		id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -31,9 +42,10 @@ func createTable() error {
 		description TEXT NOT NULL,
 		location TEXT NOT NULL,
 		dateTime DATETIME NOT NULL,
-		user_id INTEGER
+		user_id INTEGER,
+		FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
 	);
 	`
-	_, err := DB.Exec(createEventsTable)
+	_, err = DB.Exec(createEventsTable)
 	return err
 }
