f479ff56d711f78d863ff814ef1b367421b589d7
reganputra <ramadhanregan334@gmail.com>
2025-06-14 23:45:43 +0700

    add authentication middleware to validate JWT tokens and set user ID in context

---
+++ middleware/authMiddleware.go
@@ -0,0 +1,38 @@
+package middleware
+
+import (
+	"github.com/gin-gonic/gin"
+	"go-rest-api/utils"
+	"net/http"
+	"strings"
+)
+
+func AuthMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		// Get the Authorization header
+		authHeader := c.GetHeader("Authorization")
+		if authHeader == "" {
+			c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "Authorization token is required"})
+			return
+		}
+
+		// If the token is in the format "Bearer <token>", remove the "Bearer" prefix
+		token := authHeader
+		if strings.HasPrefix(authHeader, "Bearer ") {
+			token = strings.TrimPrefix(authHeader, "Bearer ")
+		}
+
+		// Validate the token
+		userId, err := utils.ValidateToken(token)
+		if err != nil {
+			c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "Invalid token"})
+			return
+		}
+
+		// Set the user ID in the context for use in handlers
+		c.Set("userId", userId)
+
+		// Continue to the next handler
+		c.Next()
+	}
+}
